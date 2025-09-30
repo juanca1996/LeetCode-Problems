cb76bcd9305aef212a9e70c4ce9a572b4ef99455
juan camilo arboleda <juancaarboleda962008@hotmail.com>
2025-09-30 16:46:37 -0500

    Longest Prefix easy solution

--- LeetCode/Easy/LongestPrefix/LongestPrefix.cs
+++ LeetCode/Easy/LongestPrefix/LongestPrefix.cs
@@ -56,5 +56,54 @@ namespace LeetCode.Easy.LongestPrefix
 
             return largestWord;
         }
+
+        public string LongestCommonPrefixEasy(string[] strs)
+        {
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            string smallwordinthelist = strs[0];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (strs[i].Length < smallwordinthelist.Length)
+                {
+                    smallwordinthelist = strs[i];
+                }
+            }
+
+            var firstValueArray = smallwordinthelist.ToArray().Select(x => x.ToString()).ToArray();
+
+            strs = strs.OrderByDescending(x => x.Length).ToArray();
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < firstValueArray.Length; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    var arraySubString = strs[j].ToArray();
+                    if (arraySubString[i].ToString() != firstValueArray[i])
+                    {
+                        if (builder.Length == 0)
+                        {
+                            return "";
+                        }
+                        else
+                        {
+                            return builder.ToString();
+                        }
+
+                    }
+                    else if ((j + 1) == strs.Length)
+                    {
+                        builder.Append(firstValueArray[i]);
+                    }
+
+                }
+            }
+
+            return builder.ToString();
+        }
     }
 }
