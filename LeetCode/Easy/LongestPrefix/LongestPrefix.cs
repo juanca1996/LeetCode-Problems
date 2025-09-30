2b8015511ce681f677c1fd63a310b103410611b8
juan camilo arboleda <juancaarboleda962008@hotmail.com>
2025-09-30 16:45:45 -0500

    Longets prefix hard solution

---
+++ LeetCode/Easy/LongestPrefix/LongestPrefix.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy.LongestPrefix
+{
+    public class LongestPrefix
+    {
+        public string LongestCommonPrefixHarder(string[] strs)
+        {
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            string firstValue = strs[0];
+
+            string[] arrayFirstValue = firstValue.ToCharArray().Select(x => x.ToString()).ToArray();
+
+            string largestWord = "";
+            StringBuilder largestCommondPrefix = new StringBuilder();
+
+            int mainCursor = 0;
+            int lastestCursorState = mainCursor;
+            while (mainCursor < arrayFirstValue.Length - 1)
+            {
+                var word = arrayFirstValue[mainCursor];
+                largestCommondPrefix.Append(word);
+
+
+                for (int i = 1; i < strs.Length; i++)
+                {
+                    var contiansWord = strs[i].Contains(largestCommondPrefix.ToString());
+
+                    if (!contiansWord)
+                    {
+                        largestCommondPrefix.Clear();
+                        mainCursor = lastestCursorState;
+                        lastestCursorState++;
+                        continue;
+                    }
+
+                    if ((i + 1) == strs.Length)
+                    {
+                        if (largestCommondPrefix.Length > largestWord.Length)
+                        {
+                            largestWord = largestCommondPrefix.ToString();
+                        }
+                    }
+                }
+
+                mainCursor++;
+            }
+
+            return largestWord;
+        }
+    }
+}
